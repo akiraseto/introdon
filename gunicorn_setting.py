92aede5e9c80ef4ce566f56470f09f384889a82c
akiraseto <akira.seto@gmail.com>
2020-06-18 23:52:11 +0900

    gunicorn, nginxの本番環境設定完了

--- gunicorn_setting.py
+++ gunicorn_setting.py
@@ -1,5 +1,12 @@
 import os
 
+# Worker Processes
+workers = 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
 # Sever Socket
 host = '0.0.0.0'
 port = os.getenv('PORT', 5000)
@@ -11,11 +18,12 @@ reload = True
 
 # Logging
 accesslog = '-'
+errorlog = '-'
 loglevel = 'debug'
+# nginxのlogで十分と判断
+# accesslog = '/var/log/gunicorn_access.log'
+# errorlog = '/var/log/gunicorn_error.log'
+# loglevel = 'info'
 
 # Proc Name
-proc_name = 'Infrastructure-Flask'
-
-# Worker Processes
-workers = 1
-worker_class = 'sync'
+proc_name = 'gunicorn_flask'
